9ce5e0e6ea1accec4548b249d1a2a0372e9ee70b
xiaomo <83387856@qq.com>
2016-05-30 14:03:28 +0800

    user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var router = require('koa-router')();
 var mongoose = require('mongoose');
-var User = require('./../models/users.server.model.js')
+var User = mongoose.model('User');
 router.get('/', function *(next) {
-    this.body = yield User.find({});
+    var users = yield User.find({});
+    console.log(users);
+    yield this.render('/users/index', {
+        title: '用户列表',
+        users: users
+    })
 });
+
+router.get('/list', function *(next) {
+    yield this.render('/users/index', {
+        title: '用户列表',
+        users: User.find({})
+    })
+});
+
 router.get('/new', function *(next) {
     yield this.render('new', {
         title: '添加用户!'
