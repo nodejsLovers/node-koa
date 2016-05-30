7c986ae73475c71bfd6c1728cfc9bc6ee170c75d
xiaomo <83387856@qq.com>
2016-05-30 14:24:06 +0800

    分离方法体

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,27 @@
 var router = require('koa-router')();
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
-router.get('/', function *(next) {
+router.get('/', list);
+
+router.get('/list', list);
+router.get('/new', add)
+
+
+function *list(next) {
     var users = yield User.find({});
     console.log(users);
     yield this.render('/users/index', {
         title: '用户列表',
         users: users
     })
-});
+}
 
-router.get('/list', function *(next) {
-    yield this.render('/users/index', {
-        title: '用户列表',
-        users: User.find({})
-    })
-});
 
-router.get('/new', function *(next) {
+function *add(next) {
     yield this.render('new', {
         title: '添加用户!'
     });
-})
+}
+
 
 module.exports = router;
