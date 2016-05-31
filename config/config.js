b0adc9da561c9226ebe40f99f25bb5599ffc8f7a
xiaomo <83387856@qq.com>
2016-05-31 10:34:05 +0800

    换个数据库

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2016/5/27.
  */
 module.exports = {
-    mongodb:"mongodb://xiaomo:xiaomo@115.29.137.34:27017/admin"
+    mongodb:"mongodb://xiaomo:xiaomo@115.29.137.34:27017/blog"
 }
 
 // module.exports = {
