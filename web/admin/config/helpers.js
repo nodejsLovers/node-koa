35d86bb7c5c8cb2812a8c29c52d78c86c9bafe84
xiaomo <83387856@qq.com>
2016-06-03 17:20:02 +0800

    帮助类

--- web/admin/config/helpers.js
+++ web/admin/config/helpers.js
@@ -1,4 +1,5 @@
 /**
+ * 帮助类
  * @author: @xiaomo
  */
 
@@ -6,16 +7,30 @@ var path = require('path');
 
 // Helper functions
 var ROOT = path.resolve(__dirname, '..');
+console.log('项目根目录:', root() + '\n');
 
-console.log('root directory:', root() + '\n');
 
+/**
+ * 运行的脚本中是否包含当前flag
+ * @param flag
+ * @returns {boolean}
+ * 包含 true
+ * 否则 false
+ */
 function hasProcessFlag(flag) {
-  return process.argv.join('').indexOf(flag) > -1;
+    return process.argv.join('').indexOf(flag) > -1;
 }
 
+/**
+ *
+ * @param args 一个类数组
+ * @returns {string}
+ * 传入一个类数组,转成数组并拼上根目录
+ */
 function root(args) {
-  args = Array.prototype.slice.call(arguments, 0);
-  return path.join.apply(path, [ROOT].concat(args));
+    args = Array.prototype.slice.call(arguments, 0);
+    console.log(path.join.apply(path, [ROOT].concat(args)));
+    return path.join.apply(path, [ROOT].concat(args));
 }
 
 
