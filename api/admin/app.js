84e45c1080b66f7434210585276c208436f4e172
xiaomo <83387856@qq.com>
2016-06-03 14:46:12 +0800

    加上views

--- api/admin/app.js
+++ api/admin/app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const app = new Koa();
 const router = require('koa-router')();
+const views = require('koa-views');
 const co = require('co');
 const convert = require('koa-convert');
 const json = require('koa-json');
@@ -16,16 +17,17 @@ require('babel-core/register')({
     presets: ['es2015-node5', 'stage-3']
 });
 
-app.use(views('views', {
-    root: __dirname + '/views',
-    default: 'ejs'
-}));
-
 // middlewares
 app.use(convert(bodyparser));
 app.use(convert(json()));
 app.use(convert(logger()));
 app.use(require('koa-static')(__dirname + '/public'));
+
+app.use(convert(views(__dirname, {
+    map: {
+        html: 'ejs'
+    }
+})));
 
 router.use('/', index.routes(), index.allowedMethods());
 router.use('/users', users.routes(), users.allowedMethods());
