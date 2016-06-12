76676895bcd97e5172371314b6fd9225f01a1a54
xiaomo <83387856@qq.com>
2016-06-12 14:44:17 +0800

    api

---
+++ web/admin/app/url/api.url.js
@@ -0,0 +1,7 @@
+const base = 'http://api.xiaomo.info/';
+
+export const findAll = base + 'admin/findAll';
+export const get = base + 'admin/findAll';
+export const del = base + 'admin/findAll';
+export const add = base + 'admin/findAll';
+export const save = base + 'admin/findAll';
